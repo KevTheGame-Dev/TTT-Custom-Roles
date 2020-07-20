e67f21f070fc94df521efb9a929a43fb07cc52bf
wtc4863 <wtc4863@g.rit.edu>
2020-07-20 15:36:09 -0400

    i forgot another comma god dammit

--- gamemodes/terrortown/gamemode/cl_lang.lua
+++ gamemodes/terrortown/gamemode/cl_lang.lua
@@ -215,7 +215,7 @@ local bgcolor = {
 	[ROLE_ASSASSIN] = Color(112, 50, 0, 200),
 	[ROLE_INNOCENT] = Color(0, 50, 0, 200),
 	[ROLE_KILLER] = Color(50, 0, 70, 200),
-	[ROLE_CANNIBAL] = Color(176, 137, 54, 200)
+	[ROLE_CANNIBAL] = Color(176, 137, 54, 200),
 	[ROLE_CROOKEDCOP] = Color(153, 51, 102, 200)
 };
 
